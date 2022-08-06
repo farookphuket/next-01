e6f24a8ae76393b2fb4d902711acee56a1aec2f4
farook <farookphuket@gmail.com>
2022-08-06 17:54:20 +0700

    edit 6 aug 2022

--- EXAMPLE.next.config.js
+++ EXAMPLE.next.config.js
@@ -2,12 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-    env.MONGO_URI:"",
-    env.GITHUB_ID:"",
-    env.GITHUB_SECRET:"",
-    env.GOOGLE_CLIENT_ID:"",
-    env.GOOGLE_CLIENT_SECRET:"",
-    env.JWT_SECRET:""
+  env:{
+    MONGO_URI:"",
+    GITHUB_ID:"",
+    GITHUB_SECRET:"",
+    GOOGLE_CLIENT_ID:"",
+    GOOGLE_CLIENT_SECRET:"",
+    JWT_SECRET:""
+  }
+
 }
 
 module.exports = nextConfig
